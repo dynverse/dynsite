91150dbef3dac18a30403a90773fe33b7760bac1
Wouter Saelens <wouter.saelens@gmail.com>
2019-03-28 21:45:00 +0100

    update script

--- scripts/script.sh
+++
@@ -1,8 +0,0 @@
-git branch -d master
-git checkout --orphan master
-git add .
-git commit -m "update site"
-git push --force --set-upstream origin master
-
-cp CNAME public/CNAME
-git push origin `git subtree split --prefix public master`:master --force
